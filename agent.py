277eeb568803b82cfcf0f5eca40d1bdf49c328dd
xiaoyu-zhou <xiaoyu331210@gmail.com>
2025-01-23 14:26:14 -0800

    implemented visual dqn, have not trained yet

--- agent.py
+++ agent.py
@@ -13,7 +13,82 @@ LR = 1e-5               # learning rate
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-class Agent():
+class VisualStateAgent():
+    def __init__(self, action_num, gamma):
+        self.action_num = action_num
+        self.gamma = gamma
+
+        # create the network
+        self.network_local = model.VisualStateDQN(action_num)
+        self.network_target = model.VisualStateDQN(action_num)
+
+        # Experience replay queue
+        self.memory = ReplayBuffer(int(1e5), BATCH_SIZE)
+
+        self.optimizer = optim.Adam(self.network_local.parameters())
+    
+    def step(self, state, action, reward, next_state, done):
+        # update memory with the current observations
+        self.memory.add(state, action, reward, next_state, done)
+        if len(self.memory) < BATCH_SIZE:
+            return
+        experiences = self.memory.sample()
+        self.__learn(experiences, self.gamma)
+    
+    def act(self, states, eps):
+        # states = torch.from_numpy(states).float().unsqueeze(0).to(device)
+        states = torch.from_numpy(states).float().to(device)
+        self.network_local.eval()
+        with torch.no_grad():
+            action_values = self.network_local(states)
+        self.network_local.train()
+
+        # do spsilon-greedy action selection
+        if random.random() > eps:
+            return np.argmax(action_values.cpu().data.numpy())
+        else:
+            return random.choice(np.arange(self.action_num))
+        
+
+    def __learn(self, experienecs, gamma):
+        # get indivdiual elements from experiences
+        states, actions, rewards, next_states, dones = experienecs
+
+        # get Q value from both target and current network
+        q_target_next_max = self.network_target(next_states).detach().max(1)[0].unsqueeze(1)
+        q_targets = rewards + (gamma * q_target_next_max * (1 - dones))
+
+        q_expected = self.network_local(states).gather(1, actions.long())
+
+        # compute loss
+        loss = F.mse_loss(q_targets, q_expected)
+
+        # optimize
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        # Soft update the target network
+        TAU = 1e-3
+        self.__soft_update(self.network_local, self.network_target, TAU)
+
+
+    def __soft_update(self, local_model, target_model, tau):
+        """Soft update model parameters.
+        θ_target = τ*θ_local + (1 - τ)*θ_target
+
+        Params
+        ======
+            local_model (PyTorch model): weights will be copied from
+            target_model (PyTorch model): weights will be copied to
+            tau (float): interpolation parameter 
+        """
+        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+            target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
+
+
+
+class DiscreteStateAgent():
     def __init__(self, state_num, action_num, gamma):
         self.state_num = state_num
         self.action_num = action_num
--- model.py
+++ model.py
@@ -16,7 +16,45 @@ class DiscreteStateDQN(nn.Module):
         x = F.relu(self.fc2(x))
         return self.fc3(x)
 
-# test = torch.rand((2, 3))
+HIDDEN_LAYER_1 = 16
+HIDDEN_LAYER_2 = 32
+class VisualStateDQN(nn.Module):
+    def __init__(self, num_action_space):
+        super(VisualStateDQN, self).__init__()
+        # the input image should be [84 x 84]
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, HIDDEN_LAYER_1, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(HIDDEN_LAYER_1))
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(HIDDEN_LAYER_1, HIDDEN_LAYER_2, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(HIDDEN_LAYER_2))
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(42*42*HIDDEN_LAYER_2, num_action_space)
+        
+    
+    def forward(self, x):
+        # The input state dimension [batch, height, width, channel]
+        # print("shape start", x.shape)
+        x = F.relu(self.conv1(x)) # [batch, 84, 84, 64]
+        # print("shape after conv1", x.shape)
+        x = F.relu(self.conv2(x)) # [batch, 84, 84, 64]
+        # print("shape after conv2", x.shape)
+        x = self.pool(x) # [batch, 42, 42, 128]
+        # print("shape after pool", x.shape)
+        x = x.view(x.size(0), -1) # [batch, ]
+        # print("shape after flatten", x.shape)
+        # print("fc1", self.fc1)
+        return self.fc1(x)
+        # x = F.relu(self.fc1(x))
+        # # print("shape after fc1", x.shape)
+        # x = F.relu(self.fc2(x))
+
+        # # print("shape after fc2", x.shape)
+        # return self.fc3(x)
+
+
+# test = torch.rand((2, 3, 4, 5))
+# print(test.view(2, 3, -1).shape)
 # print(test)
 # action = torch.tensor([[0.],[1.]])
 # action_int = action.int()
